f585ce17161c59fdae529e9586ab50f6246d8592
t_ueda <uetatsu1005@gmail.com>
2025-07-31 00:46:45 +0900

    Add comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,24 +5,31 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut randgen = rand::thread_rng(); // Initialize random number generator
+    let range = 1..101; // Range literal. Range is half-open by default in Rust
 
+    let secret_number = randgen.gen_range(range);
+
+    // In Rust, infinite loop be like this
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
+        // &mut guess is "mutable reference"
+        // It IS different from *mut guess (raw pointer), which bypasses Rust's safety checks
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line");
+            .expect("Failed to read line"); // .expect means "panic if it returns Err"
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            Ok(num) => num,     // return num if Ok
+            Err(_) => continue, // continue to the next iteration if Err
         };
 
         println!("You guessed: {}", guess);
 
+        // &secret_number is just a "reference" to secret_number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
